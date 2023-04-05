c4368c9204b802426f4e7c2a81dc67a5a7e78e60
hyunseo24 <hugemouth@naver.com>
2023-04-05 22:06:14 +0900

    create folders

---
+++ Source/studyUE4/studyUE4.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "studyUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, studyUE4, "studyUE4" );
